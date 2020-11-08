c947014f3ee82bf767323ffe802d4adb8e23357d
code-shoot <franksmithattheshoot@gmail.com>
2020-11-08 13:46:09 +0530

    almost complete

--- script.js
+++ script.js
@@ -19,11 +19,7 @@ const checkLetter = (e) => {
     let workingData = randomData;
     console.log(lives);
 
-    if(!lives){
-        console.log("Game Over");
-        gameOver.style.display = 'block';
-    }
-    else if(output.includes(e.key) || wrongInputs.includes(e.key)){
+    if(output.includes(e.key) || wrongInputs.includes(e.key)){
         // console.log('Entered Duplicate Value');
         duplicate_alert.classList.toggle('show_alert');
         // console.log(duplicate_alert.classList);
@@ -40,6 +36,10 @@ const checkLetter = (e) => {
                 notFound = false;
             }
         }   
+        if(output.length >= randomData.length) {
+            won.style.display = 'block';
+            document.removeEventListener();
+        }
         if(notFound){
             wrongInputs += e.key;
             lives--;
@@ -47,31 +47,50 @@ const checkLetter = (e) => {
             wrong_show.children[1].innerHTML +=  (wrong_show.children[1].innerHTML) ? `,${e.key}` : `${e.key}`;
             hangman.children[body_part_count].style.display = "block";
             body_part_count++;
-            console.log(body_part_count);
+            // console.log(body_part_count);
         }
     }        
+    if(!lives){
+        console.log("Game Over");
+        gameOver.style.display = 'block';
+    }
 }
 
 const reset = () => {
     lives = 6;
+    body_part_count = 4;
     output = '';
     wrongInputs = '';
     gameOver.style.display = 'none';
+    
     word.childNodes.forEach(node => {
         node.innerHTML = '';
     })
     wrong_show.children[1].innerHTML = '';
     wrong_show.style.display = 'none';
+    for(i=4; i<10; i++){
+        hangman.children[i].style.display = "none";
+    }
 }
+
+const playAgain = () => {
+    reset();
+    won.style.display = 'none';
+    randomData = data[Math.floor(Math.random()*5)];
+    word = makeWord(randomData.length);
+}
+
 
 //Main start's from here
 let duplicate_alert = document.querySelector(".duplicate_alert");
 let playAgain_btn = document.querySelector(".playAgain");
+let playAgain_btn1 = document.querySelector(".playAgain1");
 let gameOver = document.querySelector(".gameOver_container");
+let won = document.querySelector(".won_container");
 let wrong_show = document.querySelector(".wrong");
 let hangman = document.querySelector(".figure-container");
 
-let data = ['programming', 'dance', 'eliminate', 'software', 'hacker'];
+let data = ['car', 'bus', 'dance', 'rat', 'hacker', 'bat', 'walk'];
 let output = '';
 let wrongInputs = '';
 let lives = 6;
@@ -81,3 +100,4 @@ let body_part_count = 4;
 
 document.addEventListener('keypress', checkLetter);
 playAgain_btn.addEventListener('click', reset);
+playAgain_btn1.addEventListener('click', playAgain);
